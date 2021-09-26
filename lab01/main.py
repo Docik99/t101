2d26f8d59472764075063537942b4d686f712b08
docik <docik@1.ru>
2021-09-26 11:26:51 +0300

    добавлена генерация тестов

--- lab01/main.py
+++ lab01/main.py
@@ -30,30 +30,39 @@ def load_data(data_file):
     rules_dict = {}
     f_json = open(data_file, 'r')
     rules = json.load(f_json)
+
+    # for rule in rules:
+    #     condition = rule['if']
+    #     result = rule['then']
+    #     i = 0
+    #     for part in condition['and']:
+    #         i += 1
+    #         element_list = []
+    #         if isinstance(part, list):
+    #             for element in part:
+    #                 if isinstance(element, dict):
+    #                     for not_element in element['not']:
+    #                         element_list.append(not_element * -1)
+    #                 else:
+    #                     element_list.append(element)
+    #         else:
+    #             if isinstance(part, dict):
+    #                 for not_element in part['not']:
+    #                     element_list.append(not_element * -1)
+    #             else:
+    #                 element_list.append(part)
+    #
+    #         rules_dict.update({tuple(element_list): result})
+    # print(rules_dict)
+    # return rules_dict
+
     for rule in rules:
         condition = rule['if']
         result = rule['then']
-        i = 0
-        for part in condition['and']:
-            i += 1
-            element_list = []
-            if isinstance(part, list):
-                for element in part:
-                    if isinstance(element, dict):
-                        for not_element in element['not']:
-                            element_list.append(not_element * -1)
-                    else:
-                        element_list.append(element)
-            else:
-                if isinstance(part, dict):
-                    for not_element in part['not']:
-                        element_list.append(not_element * -1)
-                else:
-                    element_list.append(part)
-
-            rules_dict.update({tuple(element_list): result})
-    print(rules_dict)
-    return rules_dict
+        element_list = []
+        if isinstance(condition, list):
+            for element in condition:
+                element_list.append(element)
 
 
 def check_rule(check_file, rule):
