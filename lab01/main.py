3c169c28ebfc7c0e9c695566d02535b603b89ec3
docik <docik@1.ru>
2021-09-26 21:24:56 +0300

    рисуночки графов подъехали

--- lab01/main.py
+++ lab01/main.py
@@ -25,16 +25,16 @@ def load_data(data_file):
     :param data_file: файл с правилами
     :return: словарь с обработанными правилами
     """
-    rules_dict = {}
+    graph_list = []
     f_json = open(data_file, 'r')
     rules = json.load(f_json)
 
     for rule in rules:
         condition = rule['if']
         result = rule['then']
-        element_list = []
-        graph = nx.DiGraph()
+        #element_list = []
         for operation in condition:
+            graph = nx.DiGraph()
             if operation == 'and':
                 oper = -1
             elif operation == 'or':
@@ -43,12 +43,14 @@ def load_data(data_file):
                 oper = -3
             for element in condition[operation]:
                 graph.add_edge(element, oper)
-                element_list.append(element)
+                #element_list.append(element)
             graph.add_edge(oper, result[0])
-        rules_dict.update({tuple(element_list): result})
-        # nx.draw(graph)
-        # plt.show()
-    return rules_dict
+            graph_list.append(graph)
+        #rules_dict.update({tuple(element_list): result})
+    for g in graph_list:
+        nx.draw(g, with_labels=True)
+        plt.show()
+    return graph_list
 
 
 if __name__ == '__main__':
