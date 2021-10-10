820d7cead13cf3c8aaab07baa44a3f312c25d24a
docik <docik@1.ru>
2021-10-11 01:31:22 +0300

    изменена логика создания графа

--- lab01/main.py
+++ lab01/main.py
@@ -33,25 +33,38 @@ def load_data(data_file):
     """
     r_json = open(data_file, 'r')
     rules = json.load(r_json)
-    count_rules = len(rules)
-    oper = 0
+    count_not_rules = 0
+    oper_and = 0.0
+    oper_not = 0
     graph = nx.MultiDiGraph()
 
     for rule in rules:
         condition = rule['if']
         result = rule['then']
-        oper -= 1
         for operation in condition:
 
-            if operation == 'and' or operation == 'not':
+            if operation == 'and':
+                oper_and -= 0.1
                 for element in condition[operation]:
-                    graph.add_edge(element, oper, log=operation)
-                    graph.add_edge(oper, element, log=None)
+                    graph.add_edge(element, oper_and, log=operation)
+                    graph.add_edge(oper_and, element, log=None)
 
                 if isinstance(result, list):
-                    graph.add_edge(oper, result[0], log=operation)
+                    graph.add_edge(oper_and, result[0], log=operation)
                 else:
-                    graph.add_edge(oper, result, log=operation)
+                    graph.add_edge(oper_and, result, log=operation)
+
+            if operation == 'not':
+                count_not_rules += 1
+                oper_not -= 1
+                for element in condition[operation]:
+                    graph.add_edge(element, oper_not, log=operation)
+                    graph.add_edge(oper_not, element, log=None)
+
+                if isinstance(result, list):
+                    graph.add_edge(oper_not, result[0], log=operation)
+                else:
+                    graph.add_edge(oper_not, result, log=operation)
 
             elif operation == 'or':
                 for element in condition[operation]:
@@ -61,15 +74,15 @@ def load_data(data_file):
                     else:
                         graph.add_edge(element, result[0], log=operation)
 
-    return graph, count_rules
+    return graph, count_not_rules
 
 
-def check_rule(graph, facts, count_rules):
+def check_rule(graph, facts, count_not_rules):
     """
     Проверка фактов по внесенным правилам
     :param graph: ориентированный мультиграф
     :param facts: список фактов
-    :param count_rules: количество правил
+    :param count_not_rules: количество правил
     :return:
         facts: новый список фактов
     """
@@ -95,22 +108,22 @@ def check_rule(graph, facts, count_rules):
                     if op not in facts:
                         facts.append(op)
 
-    # for edge in range(count_rules * -1, 0):
-    #     for nbr in graph[edge]:
-    #         if graph.has_edge(edge, nbr):
-    #             if nbr not in facts:
-    #                 if graph[edge][nbr][0]['log'] == 'not':
-    #                     if isinstance(graph[nbr], list):
-    #                         for dubl_op in graph[nbr]:
-    #                             for dubl_el in graph[dubl_op]:
-    #                                 if dubl_el not in facts:
-    #                                     if graph[dubl_op][dubl_el][0]['log'] == 'not':
-    #                                         facts.append(dubl_el)
-    #                     else:
-    #                         if graph[edge][nbr][0]['log'] == 'not':
-    #                             facts.append(nbr)
-    #             else:
-    #                 break
+    for edge in range(count_not_rules * -1, 0):
+        for nbr in graph[edge]:
+            if graph.has_edge(edge, nbr):
+                if nbr not in facts:
+                    if graph[edge][nbr][0]['log'] == 'not':
+                        if isinstance(graph[nbr], list):
+                            for dubl_op in graph[nbr]:
+                                for dubl_el in graph[dubl_op]:
+                                    if dubl_el not in facts:
+                                        if graph[dubl_op][dubl_el][0]['log'] == 'not':
+                                            facts.append(dubl_el)
+                        else:
+                            if graph[edge][nbr][0]['log'] == 'not':
+                                facts.append(nbr)
+                else:
+                    break
 
     return facts
 
@@ -121,14 +134,14 @@ if __name__ == '__main__':
 
     time_start = time()
 
-    rules, count_rules = load_data(args.file)
+    rules, count_not_rules = load_data(args.file)
     nx.draw(rules, with_labels=True)
     plt.show()
 
     print(time() - time_start)
 
     time_start = time()
-    answer = check_rule(rules, [8, 9, 13, 99, 112, 114], count_rules)
+    answer = check_rule(rules, [8, 9, 13, 99, 112, 114], count_not_rules)
     print(time() - time_start)
 
     print(answer)
